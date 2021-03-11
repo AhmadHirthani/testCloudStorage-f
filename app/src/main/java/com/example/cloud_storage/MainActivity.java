d254c93e0eb2fbdf8929bbafab0c412f77029a61
Ahmad <hertani86@gmail.com>
2021-03-11 18:18:52 +0200

    add String

--- app/src/main/java/com/example/cloud_storage/MainActivity.java
+++ app/src/main/java/com/example/cloud_storage/MainActivity.java
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView imageView;
     Uri filePath;
     StorageReference firebaseStorage;
+    String x;
 
 
 
